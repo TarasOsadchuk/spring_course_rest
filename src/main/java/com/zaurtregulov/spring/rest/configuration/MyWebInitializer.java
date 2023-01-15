fa253e48e66f9325eeaf3ab086e0dc2fc50ec433
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-15 09:45:40 +0200

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/rest/configuration/MyWebInitializer.java
@@ -0,0 +1,18 @@
+package com.zaurtregulov.spring.rest.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MyConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,34 @@
+package com.zaurtregulov.spring.rest.controller;
+
+import com.zaurtregulov.spring.rest.entity.Employee;
+import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.zaurtregulov.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+        return employee;
+    }
+}
