740cdf095d9c9698fc0cc480b2d167d34307fd89
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-15 11:33:48 +0200

    Add delete/update/add

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -25,10 +25,36 @@ public class MyRESTController {
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
 
-        if (employee==null) {
+        if (employee == null) {
             throw new NoSuchEmployeeException("There is no employee with ID = " +
                     id + " in Database");
         }
         return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + " in Database");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = " + id + " was deleted";
     }
 }
